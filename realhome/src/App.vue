ddf868caea76a5421299cfeac654c632c300042b
lhn1208@naver.com <you@example.com>
2024-05-30 17:16:43 +0900

    리얼하우스

---
+++ realhome/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Main from './main/Main.vue'
+</script>
+
+<template>
+  <div>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+    <Main />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ realhome/src/main/NoticeBox.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps, watch } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  noticeTit: {
+    type: Array,
+    required: false,
+    default: () => []
+  },
+});
+
+watch(() => props.noticeTit, (newVal) => {
+  console.log('noticeTit:', newVal);
+}, { immediate: true });
+</script>
+
+<template>
+  <div class="box">
+    <h2 class="row">{{ title }}</h2>
+    <div class="cont">
+      <div v-for="(notice, index) in noticeTit" :key="index">
+        <a href="#">{{ notice.text }} <i v-if="notice.isNew" class="ico"> NEW</i></a>
+        <span class="date">{{ notice.date }}</span>
+      </div>
+    </div>
+    <button class="more">더보기</button>
+  </div>
+</template>
